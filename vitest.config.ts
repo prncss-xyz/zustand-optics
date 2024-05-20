f803846a19853558295edcb881bfee39b47053c3
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-05-20 08:59:03 -0400

    chore: lint on commitmsg

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      include: ["lib/**"],
       reporter: ["text", "json", "clover", "lcov"],
     },
     globals: true,
